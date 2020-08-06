09797f350c6f89a31ac7973cee8b0a5ed3743dc7
Яковлев Александр Сергеевич <ASYakovlev@sberbank.ru>
2020-08-06 17:47:42 +0300

    auth

--- src/pages/Reg/Reg.ts
+++ src/pages/Reg/Reg.ts
@@ -1,7 +1,16 @@
 import { Block } from "../../modules/Block";
 import { MainFormLayout } from "../../layouts/MainForm";
 import { Form } from "../../components/UI/Form";
+import { HTTP, METHODS } from "../../utils/request";
+import { BASE_URL } from "../../consts";
 
+const req = new HTTP();
+
+const res = req.request(`${BASE_URL}/auth/signup`, {
+  method: METHODS.POST,
+});
+
+res.then((value) => console.log(value));
 const ctx = {
   label: "Регистрация",
   submit: "Зарегистрироваться",
@@ -27,8 +36,7 @@ const ctx = {
       size: "30",
       required: true,
       placeholder: "Пароль",
-      pattern:
-        "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$",
+      pattern: "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$",
     },
     {
       name: "confirm_pwd",
@@ -44,9 +52,7 @@ export class RegPage extends Block {
   render(): HTMLElement {
     const Layout = new MainFormLayout();
     const LoginForm = new Form(ctx);
-    Layout.getContent()
-      .querySelector("#main-form")
-      .appendChild(LoginForm.getContent());
+    Layout.getContent().querySelector("#main-form").appendChild(LoginForm.getContent());
     return Layout.getContent();
   }
 }
